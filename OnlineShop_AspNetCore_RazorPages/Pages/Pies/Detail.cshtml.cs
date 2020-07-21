41edd3224ccca59640d257093b7e834ba41028fe
Harish Sethi <sethi.harish@gmail.com>
2020-07-21 12:37:31 +1000

    Add Detail Page

---
+++ OnlineShop_AspNetCore_RazorPages/Pages/Pies/Detail.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineShop.Data.Services;
+using OnlineShop.Entities;
+using System.Threading.Tasks;
+
+namespace OnlineShop_AspNetCore_RazorPages.Pages.Pies
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IPieService pieService;
+
+        public DetailModel(IPieService pieService)
+        {
+            this.pieService = pieService;
+        }
+
+        public Pie Pie { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Pie = await pieService.GetPieAsync(id);
+        }
+    }
+}
