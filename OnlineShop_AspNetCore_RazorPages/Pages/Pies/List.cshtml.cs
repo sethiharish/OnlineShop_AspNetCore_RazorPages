63e100b0094fc871158325e1760ccde24af58d02
Harish Sethi <sethi.harish@gmail.com>
2020-07-21 20:34:36 +1000

    Add List Page

---
+++ OnlineShop_AspNetCore_RazorPages/Pages/Pies/List.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineShop.Data.Services;
+using OnlineShop.Entities;
+
+namespace OnlineShop_AspNetCore_RazorPages.Pages.Pies
+{
+    public class ListModel : PageModel
+    {
+        private readonly IPieService pieService;
+        private readonly ICategoryService categoryService;
+
+        public ListModel(IPieService pieService, ICategoryService categoryService)
+        {
+            this.pieService = pieService;
+            this.categoryService = categoryService;
+        }
+        
+        public IEnumerable<Pie> Pies { get; set; }
+
+        public string SelectedCategoryName { get; set; }
+
+        public async Task OnGet(string category)
+        {
+            Category selectedCategory = null;
+            if (!string.IsNullOrEmpty(category))
+            {
+                selectedCategory = await categoryService.GetCategoryByNameAsync(category);
+            }
+            if(selectedCategory == null)
+            {
+                Pies = await pieService.GetPiesAsync();
+            }
+            else
+            {
+                Pies = await pieService.GetPiesByCategoryAsync(category);
+                SelectedCategoryName = selectedCategory.Name;
+            }
+        }
+    }
+}
---
+++ OnlineShop_AspNetCore_RazorPages/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data.Services;
+
+namespace OnlineShop_AspNetCore_RazorPages.ViewComponents
+{
+    public class CategoryListViewComponent : ViewComponent
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryListViewComponent(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = (categoryService.GetCategoriesAsync()).Result;
+            return View(categories);
+        }
+    }
+}
